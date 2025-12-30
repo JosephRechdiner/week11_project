d5f989f0d55e4c66893084c665e24b64fbbad171
JosephRer <jrechdiner2002@gmail.com>
2025-12-30 12:19:15 +0200

    finished init and added mongo connector

---
+++ app/database/connector.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import os
+
+load_dotenv()
+
+MONGO_HOST=os.getenv("MONGO_HOST")
+MONGO_PORT=os.getenv("MONGO_PORT")
+MONGO_DB=os.getenv("MONGO_DB")
+
+MONGO_URI = f'mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}'
+
+def get_db():
+    try:
+        client = MongoClient(MONGO_URI)
+        db = client[MONGO_DB]
+        yield db
+    finally:
+        client.close()
