c966d9c84b4aacbd8dea758fb17f9346b5db0a19
JosephRer <jrechdiner2002@gmail.com>
2025-12-31 09:24:19 +0200

    changed mongodb connection class

--- app/database/connector.py
+++ app/database/connector.py
@@ -10,11 +10,43 @@ MONGO_PORT=os.getenv("MONGO_PORT")
 MONGO_DB=os.getenv("MONGO_DB")
 
 MONGO_URI = f'mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}'
+MONGO_COLLECTION = "contacts"
 
-def get_db():
-    try:
-        client = MongoClient(MONGO_URI)
-        db = client[MONGO_DB]
-        return db
-    except Exception as e:
-        raise e
+class MongoDB:
+    def __init__(self, db=MONGO_DB, uri=MONGO_URI, collection=MONGO_COLLECTION):
+        self.db = db
+        self.uri = uri
+        self.collection = collection
+        self.client = None
+
+    def get_collection(self):
+        try:
+            if not self.client:
+                self.client = self.get_client()
+                db = self.client[self.db]
+                return db[self.collection]
+        except Exception as e:
+            raise e
+        
+    def get_db(self):
+        try:
+            if not self.client:
+                self.client = self.get_client()
+                db = self.client[self.db]
+                return db
+        except Exception as e:
+            raise e
+
+    def get_client(self):
+        try:
+            if not self.client:
+                self.client = MongoClient(self.uri)
+            return self.client
+        except Exception as e:
+            raise e
+        
+    def close_connection(self):
+        try:
+            self.client.close()
+        except Exception as e:
+            raise e
