5577b25e2500731c31387de5adedf4d9063e6584
JosephRer <jrechdiner2002@gmail.com>
2025-12-31 09:26:47 +0200

    change mongodb class structure

--- app/database/connector.py
+++ app/database/connector.py
@@ -19,24 +19,6 @@ class MongoDB:
         self.collection = collection
         self.client = None
 
-    def get_collection(self):
-        try:
-            if not self.client:
-                self.client = self.get_client()
-                db = self.client[self.db]
-                return db[self.collection]
-        except Exception as e:
-            raise e
-        
-    def get_db(self):
-        try:
-            if not self.client:
-                self.client = self.get_client()
-                db = self.client[self.db]
-                return db
-        except Exception as e:
-            raise e
-
     def get_client(self):
         try:
             if not self.client:
@@ -48,5 +30,24 @@ class MongoDB:
     def close_connection(self):
         try:
             self.client.close()
+        except Exception as e:
+            raise e
+        
+        
+    def get_db(self):
+        try:
+            if not self.client:
+                self.client = self.get_client()
+                db = self.client[self.db]
+                return db
+        except Exception as e:
+            raise e
+
+    def get_collection(self):
+        try:
+            if not self.client:
+                self.client = self.get_client()
+                db = self.client[self.db]
+                return db[self.collection]
         except Exception as e:
             raise e
